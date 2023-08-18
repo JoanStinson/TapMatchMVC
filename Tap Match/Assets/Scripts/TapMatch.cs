7ce8eac8fd355cb584bd262655b31d09cac40f49
Joan Ginard Mateo <jgm7.hd@gmail.com>
2023-08-18 15:56:22 +0200

    first classes and added moq

---
+++ Tap Match/Assets/Scripts/TapMatch.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+namespace JGM.Game
+{
+    public class TapMatch : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Run();
+        }
+
+        private void Run()
+        {
+            
+        }
+    }
+}
