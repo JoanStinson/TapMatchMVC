4b7ff402ccb61b5d48e8d5ff0fc090c32af18a26
Joan Ginard Mateo <jgm7.hd@gmail.com>
2023-08-18 16:49:04 +0200

    coordinate test

--- Tap Match/Assets/Scripts/TapMatch.cs
+++ Tap Match/Assets/Scripts/TapMatch.cs
@@ -52,4 +52,38 @@ namespace JGM.Game
             m_gameController = gameController;
         }
     }
+
+    public class Coordinate
+    {
+        public int X => m_x;
+        public int Y => m_y;
+        public Color Color => m_color;
+        
+        private int m_x;
+        private int m_y;
+        private Color m_color;
+
+        public Coordinate(int x, int y, Color color)
+        {
+            m_x = x;
+            m_y = y;
+            m_color = color;
+        }
+
+        public bool IsConnected(Coordinate newCoordinate)
+        {
+            if (m_color != newCoordinate.m_color)
+            {
+                return false;
+            }
+
+            int rowDiff = Math.Abs(m_x - newCoordinate.m_x);
+            int colDiff = Math.Abs(m_y - newCoordinate.m_y);
+
+            bool connectedVertically = (rowDiff == 1 && colDiff == 0);
+            bool connectedHorizontally = (rowDiff == 0 && colDiff == 1);
+
+            return connectedVertically || connectedHorizontally;
+        }
+    }
 }
--- Tap Match/Assets/Tests/TapMatchTest.cs
+++ Tap Match/Assets/Tests/TapMatchTest.cs
@@ -31,4 +31,35 @@ namespace JGM.GameTests
             m_gameControllerMock.Verify(mock => mock.Initialize(), Times.Once());
         }
     }
+
+    public class CoordinateTest
+    {
+        private Coordinate m_coordinate;
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_coordinate = new Coordinate(1, 1, Color.white);
+        }
+
+        /* 0 X 0
+         * X X X
+         * 0 X 0
+         * Only vertically and horizontally
+         */
+        [TestCase(false, 0, 0)]
+        [TestCase(true, 0, 1)]
+        [TestCase(false, 0, 2)]
+        [TestCase(true, 1, 0)]
+        [TestCase(true, 1, 2)]
+        [TestCase(false, 2, 0)]
+        [TestCase(true, 2, 1)]
+        [TestCase(false, 2, 2)]
+        public void When_IsConnectedIsCalled_ReturnsExpectedValue(bool expectedResult, params int[] coordinates)
+        {
+            var newCoordinate = new Coordinate(coordinates[0], coordinates[1], Color.white);
+            bool actualResult = m_coordinate.IsConnected(newCoordinate);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
 }
