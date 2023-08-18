256eaba3ec6e702669fc48199dd83763e8b334d2
Joan Ginard Mateo <jgm7.hd@gmail.com>
2023-08-18 17:58:37 +0200

    grid model test

--- Tap Match/Assets/Scripts/TapMatch.cs
+++ Tap Match/Assets/Scripts/TapMatch.cs
@@ -56,9 +56,14 @@ namespace JGM.Game
     public class CellModel
     {
         private Coordinate m_coordinate;
-        private Color m_color;
+        private Color m_color = Color.gray;
 
         public CellModel(Coordinate coordinate, Color color)
+        {
+            SetCell(coordinate, color);
+        }
+
+        public void SetCell(Coordinate coordinate, Color color)
         {
             m_coordinate = coordinate;
             m_color = color;
@@ -73,12 +78,21 @@ namespace JGM.Game
 
             return m_coordinate.IsAdjacent(otherCell.m_coordinate);
         }
+
+        public void EmptyCell()
+        {
+            m_coordinate.Reset();
+            m_color = Color.gray;
+        }
     }
 
     public class Coordinate
     {
-        private int m_x;
-        private int m_y;
+        public int x => m_x;
+        public int y => m_y;
+
+        private int m_x = -1;
+        private int m_y = -1;
 
         public Coordinate(int x, int y)
         {
@@ -91,6 +105,42 @@ namespace JGM.Game
             int rowDiff = Math.Abs(m_x - otherCoordinate.m_x);
             int colDiff = Math.Abs(m_y - otherCoordinate.m_y);
             return (rowDiff == 1 && colDiff == 0) || (rowDiff == 0 && colDiff == 1);
+        }
+
+        public void Reset()
+        {
+            m_x = -1;
+            m_y = -1;
+        }
+    }
+
+    public class GridModel
+    {
+        private CellModel[,] m_grid;
+
+        private const int m_minSize = 5;
+        private const int m_maxSize = 20;
+
+        public GridModel(int numberOfRows, int numberOfCols)
+        {
+            Debug.Assert(numberOfRows >= m_minSize && numberOfRows <= m_maxSize);
+            Debug.Assert(numberOfCols >= m_minSize && numberOfCols <= m_maxSize);
+            m_grid = new CellModel[numberOfRows, numberOfCols];
+        }
+
+        public CellModel SetCell(Coordinate coordinate, Color color)
+        {
+            //Debug.Assert(coordinate.x >= 0 && coordinate.x <= m_grid.GetLength(0));
+            //Debug.Assert(coordinate.y >= 0 && coordinate.y <= m_grid.GetLength(1));
+            m_grid[coordinate.x, coordinate.y].SetCell(coordinate, color);
+            return m_grid[coordinate.x, coordinate.y];
+        }
+
+        public void EmptyCell(Coordinate coordinate)
+        {
+            Debug.Assert(coordinate.x >= 0 && coordinate.x <= m_grid.GetLength(0));
+            Debug.Assert(coordinate.y >= 0 && coordinate.y <= m_grid.GetLength(1));
+            m_grid[coordinate.x, coordinate.y].EmptyCell();
         }
     }
 }
--- Tap Match/Assets/Tests/TapMatchTest.cs
+++ Tap Match/Assets/Tests/TapMatchTest.cs
@@ -1,6 +1,7 @@
 using JGM.Game;
 using Moq;
 using NUnit.Framework;
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -92,6 +93,37 @@ namespace JGM.GameTests
             var newCellModel = new CellModel(new Coordinate(xCoord, yCoord), color);
             bool actualResult = m_cellModel.IsConnected(newCellModel);
             Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+
+    public class GridModelTest
+    {
+        private GridModel m_gridModel;
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_gridModel = new GridModel(5, 10);
+        }
+
+        [TestCase(3, 8)]
+        [TestCase(6, 1)]
+        [TestCase(5, 3)]
+        [TestCase(14, 25)]
+        public void When_GridIsCreatedWithIncorrectSize_Expect_AssertError(int rows, int cols)
+        {
+            m_gridModel = new GridModel(rows, cols);
+            LogAssert.Expect(LogType.Assert, "Assertion failed");
+        }
+
+        [TestCase(-1, 8)]
+        [TestCase(7, 12)]
+        [TestCase(6, 11)]
+        [TestCase(10, 9)]
+        public void When_SetCellIsCalledWithIncorrectSize_Expect_AssertError(int xCoord, int yCoord)
+        {
+            //LogAssert.Expect(LogType.Assert, "Assertion failed");
+            Assert.Throws<IndexOutOfRangeException>(() => m_gridModel.SetCell(new Coordinate(xCoord, yCoord), Color.white));
         }
     }
 }
