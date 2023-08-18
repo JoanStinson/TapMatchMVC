bf323eaea20818136ce6f86d30e0182c91a7c908
Joan Ginard Mateo <jgm7.hd@gmail.com>
2023-08-18 16:16:44 +0200

    first test

--- Tap Match/Assets/Scripts/TapMatch.cs
+++ Tap Match/Assets/Scripts/TapMatch.cs
@@ -3,16 +3,53 @@ using UnityEngine;
 
 namespace JGM.Game
 {
-    public class TapMatch : MonoBehaviour
+    public interface IGameDependency
     {
-        private void Awake()
-        {
-            Run();
-        }
+        void Initialize();
+    }
 
-        private void Run()
+    public interface IGameView : IGameDependency
+    {
+
+    }
+
+    public interface IGameController : IGameDependency
+    {
+
+    }
+
+    public class GameView : MonoBehaviour, IGameView
+    {
+        public void Initialize()
         {
             
+        }
+    }
+
+    public class GameController : IGameController
+    {
+        public void Initialize()
+        {
+            
+        }
+    }
+
+    public class TapMatch : MonoBehaviour
+    {
+        [SerializeField] private IGameView m_gameView;
+        private IGameController m_gameController;
+
+        private void Start()
+        {
+            m_gameView.Initialize();
+            m_gameController ??= new GameController();
+            m_gameController.Initialize();
+        }
+
+        public void SetDependencies(IGameView gameView, IGameController gameController)
+        {
+            m_gameView = gameView;
+            m_gameController = gameController;
         }
     }
 }
--- Tap Match/Assets/Tests/TapMatchTest.cs
+++ Tap Match/Assets/Tests/TapMatchTest.cs
@@ -1,21 +1,34 @@
+using JGM.Game;
+using Moq;
+using NUnit.Framework;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace JGM.GameTests
 {
     public class TapMatchTest
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        private TapMatch m_tapMatch;
+        private Mock<IGameView> m_gameViewMock;
+        private Mock<IGameController> m_gameControllerMock;
 
+        [SetUp]
+        public void SetUp()
+        {
+            var dummyGO = new GameObject("Dummy");
+            m_tapMatch = dummyGO.AddComponent<TapMatch>();
+            m_gameViewMock = new Mock<IGameView>();
+            m_gameControllerMock = new Mock<IGameController>();
+            m_tapMatch.SetDependencies(m_gameViewMock.Object, m_gameControllerMock.Object);
         }
 
-        // Update is called once per frame
-        void Update()
+        [UnityTest]
+        public IEnumerator When_StartIsCalled_InitializeMethodsGetCalledOnce()
         {
-
+            yield return null;
+            m_gameViewMock.Verify(mock => mock.Initialize(), Times.Once());
+            m_gameControllerMock.Verify(mock => mock.Initialize(), Times.Once());
         }
     }
 }
