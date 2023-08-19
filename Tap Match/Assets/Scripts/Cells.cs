4abb038ffe5d5ef1476e2692059f5fb78cecf923
Joan Ginard Mateo <jgm7.hd@gmail.com>
2023-08-19 09:46:10 +0200

    Objects in grid 3 to 6 (SO configurable)

--- Tap Match/Assets/Scripts/Cells.cs
+++ Tap Match/Assets/Scripts/Cells.cs
@@ -5,8 +5,12 @@ namespace JGM.Game
     [CreateAssetMenu(fileName = "Cells", menuName = "Settings/Cells", order = 0)]
     public class Cells : ScriptableObject
     {
-        [SerializeField] private Color[] m_colors;
+        [SerializeField] private Color[] m_colors = new Color[3];
 
-        public Color[] colors => m_colors;
+        public Color[] colors
+        {
+            get { return m_colors; }
+            set { m_colors = value; }
+        }
     }
 }
