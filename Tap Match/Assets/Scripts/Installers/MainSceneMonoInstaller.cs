325359566d1c05f42840abdf688e184d92ad8237
Joan Ginard Mateo <jgm7.hd@gmail.com>
2023-08-20 13:34:42 +0200

    Update MainSceneMonoInstaller.cs

--- Tap Match/Assets/Scripts/Installers/MainSceneMonoInstaller.cs
+++ Tap Match/Assets/Scripts/Installers/MainSceneMonoInstaller.cs
@@ -5,13 +5,26 @@ namespace JGM.Game
 {
     public class MainSceneMonoInstaller : MonoInstaller
     {
+        [Header("Services")]
         [SerializeField] private AudioService m_audioServiceInstance;
+
+        [Header("Prefabs")]
         [SerializeField] private CellView m_cellViewPrefab;
         [SerializeField] private Transform m_cellSlotPrefab;
 
         public override void InstallBindings()
         {
+            BindServices();
+            BindPrefabs();
+        }
+
+        private void BindServices()
+        {
             Container.Bind<IAudioService>().FromInstance(m_audioServiceInstance);
+        }
+
+        private void BindPrefabs()
+        {
             Container.BindFactory<CellView, CellView.Factory>().FromComponentInNewPrefab(m_cellViewPrefab);
             Container.BindFactory<Transform, CellView.SlotFactory>().FromComponentInNewPrefab(m_cellSlotPrefab);
         }
